b3bbcd0cb514967d6460e89a713779a5cb5f80a8
yakudzaa4ever <yakudzaax@gmail.com>
2025-02-17 13:01:51 +0500

    This project was not auth, so I removed them

---
+++ app/Http/Requests/AuthRegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:5',
+            'username' => 'required|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'avatar' => 'required|mimes:png,jpg,jpeg|max:2048',
+
+        ];
+    }
+}
