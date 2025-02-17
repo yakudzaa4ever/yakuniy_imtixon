f6496d09127444826b7229de9afdcaa049decd73
yakudzaa4ever <yakudzaax@gmail.com>
2025-02-17 13:12:19 +0500

    I also added user related information

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+@section('content')
+<div class="flex-grow flex items-center justify-center">
+        <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+            <h1 class="text-2xl font-bold mb-6 text-center">Login</h1>
+            <form  action="{{route('login')}}"  method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                    <input type="email" id="email" name="email" required
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <input type="password" id="password" name="password" required
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+                </div>
+                <button type="submit"
+                    class="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
+            </form>
+            <p class="mt-4 text-center text-sm text-gray-600">
+                Don't have an account? <a href="{{route('registerForm')}}"
+                    class="text-indigo-600 hover:text-indigo-500">Register</a>
+            </p>
+        </div>
+    </div>
+@endsection
